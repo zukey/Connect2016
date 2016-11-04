95bb3c0047424a3d681e700d8325bfb2c5728890
zukey <valenciada2000@gmail.com>
2016-11-05 02:21:17 +0900

    DBテーブル作成

---
+++ PictureStampRally/PictureStampRally.WebApi/Models/ImageApproximate/ScoreCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PictureStampRally.WebApi.Models.ImageApproximate
+{
+    /// <summary>
+    /// 画像の近似スコアを計算します。
+    /// </summary>
+    public class ScoreCalculator
+    {
+        /// <summary>
+        /// 指定された画像データの近似スコアを計算します。
+        /// </summary>
+        /// <param name="image1"></param>
+        /// <param name="image2"></param>
+        /// <returns>スコア</returns>
+        public int CalcApproximateScore(byte[] image1, byte[] image2)
+        {
+            return 0;
+        }
+    }
+}
