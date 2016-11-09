7e2aa11cbf942da4d1f7b5c676d10f0bd8499a68
zukey <valenciada2000@gmail.com>
2016-11-09 11:09:14 +0900

    スコア確認画面用のViewModelを追加

---
+++ PictureStampRally/PictureStampRally/ViewModels/ViewScorePageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PictureStampRally.Models;
+
+
+namespace PictureStampRally.ViewModels
+{
+    public class ViewScorePageViewModel : NotificationBase
+    {
+        IEnumerable<ThemeInfo> _themeList;
+        public IEnumerable<ThemeInfo> ThemeList
+        {
+            get { return _themeList; }
+            private set
+            {
+                SetProperty(ref _themeList, value);
+            }
+        }
+
+        ThemeInfo _selectedTheme;
+        public ThemeInfo SelectedTheme
+        {
+            get { return _selectedTheme; }
+            set
+            {
+                SetProperty(ref _selectedTheme, value);
+            }
+        }
+
+
+        public async Task LoadThemeList()
+        {
+            using (var api = new PictureStampRallyWebApi())
+            {
+                ThemeList = await api.ThemeInfo.GetAsync(1);
+
+                if (ThemeList.Any())
+                {
+                    SelectedTheme = ThemeList.First();
+                }
+            }
+        }
+
+    }
+}
