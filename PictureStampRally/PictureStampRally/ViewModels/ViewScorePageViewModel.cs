23d2cc316cab0863277295287cfa75df393239f0
zukey <valenciada2000@gmail.com>
2016-11-12 21:07:09 +0900

    コメント修正

--- PictureStampRally/PictureStampRally/ViewModels/ViewScorePageViewModel.cs
+++ PictureStampRally/PictureStampRally/ViewModels/ViewScorePageViewModel.cs
@@ -9,32 +9,55 @@ using Windows.Storage;
 
 namespace PictureStampRally.ViewModels
 {
+    /// <summary>
+    /// 撮影したデータの確認画面用のViewModel
+    /// </summary>
     public class ViewScorePageViewModel : NotificationBase
     {
+        /// <summary>
+        /// 画面遷移時に渡されたパラメータ
+        /// </summary>
         ViewScorePageParameter _param;
 
+        #region スコア
         private int? _score;
+
+        /// <summary>
+        /// スコアを取得します。
+        /// </summary>
         public int? Score
         {
             get { return _score; }
-            set
+            private set
             {
                 SetProperty(ref _score, value);
             }
         }
+        #endregion
 
 
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        /// <param name="param">画面遷移時パラメータ</param>
+        /// <returns></returns>
         public async Task Initialize(ViewScorePageParameter param)
         {
+            // パラメータ保存
             _param = param;
+
             try
             {
-                var f = await StorageFile.GetFileFromPathAsync(param.CaptureImageFilePath);
+                // 撮影したファイルを取得
+                var file = await StorageFile.GetFileFromPathAsync(param.CaptureImageFilePath);
 
                 using (var api = new PictureStampRallyWebApi())
-                using (var stream = await f.OpenStreamForReadAsync())
+                using (var stream = await file.OpenStreamForReadAsync())
                 {
+                    // チェック
                     var result = await api.Score.CheckAsync(stream, param.ThemeImageId);
+
+                    // スコア更新
                     Score = result.Score;
                 }
             }
@@ -45,16 +68,21 @@ namespace PictureStampRally.ViewModels
             }
         }
 
+        /// <summary>
+        /// 登録処理
+        /// </summary>
+        /// <returns></returns>
         public async Task Regist()
         {
-            var f = await StorageFile.GetFileFromPathAsync(_param.CaptureImageFilePath);
+            // 撮影したファイルを取得
+            var file = await StorageFile.GetFileFromPathAsync(_param.CaptureImageFilePath);
 
             using (var api = new PictureStampRallyWebApi())
-            using (var stream = await f.OpenStreamForReadAsync())
+            using (var stream = await file.OpenStreamForReadAsync())
             {
+                // 登録処理
                 var result = await api.Score.RegistAsync(stream, _param.ThemeImageId);
             }
         }
-
     }
 }
