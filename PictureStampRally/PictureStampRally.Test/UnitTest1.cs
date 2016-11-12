7e2e1c2ad94ffa4ef43bf140a05fec97f5047729
murata <murata@MURATA-PC>
2016-11-12 17:58:09 +0900

    TODO箇所実装

--- PictureStampRally/PictureStampRally.Test/UnitTest1.cs
+++ PictureStampRally/PictureStampRally.Test/UnitTest1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using PictureStampRally.WebApi.Models;
 using PictureStampRally.WebApi.Models.ImageApproximate;
+
 
 namespace PictureStampRally.Test
 {
@@ -11,12 +13,29 @@ namespace PictureStampRally.Test
         [TestMethod]
         public void スコア化テスト()
         {
-            // TODO: 頑張ってスコア化
-            var testTarget = new ScoreCalculator();
+            try
+            {
+                //　ローカルの画像のURL
+                String url1 = "C:/Users/murata/Pictures/Test/test1.jpg";
+                String url2 = "C:/Users/murata/Pictures/Test/test3.jpg";
 
-            var score = testTarget.CalcApproximateScore(new byte[0], new byte[0]);
+                // 画像の取得
+                WebClient wc = new WebClient();
+                byte[] picture1 = wc.DownloadData(url1);
 
-            Console.WriteLine(score);
+                byte[] picture2 = wc.DownloadData(url2);
+
+                // スコアの取得
+                var testTarget = new ScoreCalculator();
+                var score = testTarget.CalcApproximateScore(picture1, picture2);
+
+                Console.WriteLine(score);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
         }
     }
 }
